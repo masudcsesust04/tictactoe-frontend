7f3d53f549cb630ef2f95b7f2bdd04cf635a1aa7
Md. Masud Rana <masudcsesust04@gmail.com>
2020-08-18 16:41:49 +0600

    - Added VERSION file to maintain source code release version - Added release shell file to push and tag release commit and tag

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+# ensure we're up to date
+git pull origin master
+
+version=`cat VERSION`
+echo "version: $version"
+
+# git repository release tag
+git add -A
+git commit -m "Release version $version"
+git tag -a "v$version" -m "Relaase version $version"
+git push origin master
+git push --tags
